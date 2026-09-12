a5610dfa40844d7f1fbc0414a94d758a8d9e4424
yzyzpwdt2 <yzyzpwdt2@gmail.com>
2026-09-12 17:11:09 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-window.SCHEDULE_API_URL = "https://script.google.com/macros/s/AKfycbzetEa0ktA9OKWiSbz7EzCnfWErce09NcwBW_Ul4ZiFcymS3eqs-2JVsoSTLfQ7p6kx/exec;
+window.SCHEDULE_API_URL = "https://script.google.com/macros/s/AKfycbzetEa0ktA9OKWiSbz7EzCnfWErce09NcwBW_Ul4ZiFcymS3eqs-2JVsoSTLfQ7p6kx/exec";
